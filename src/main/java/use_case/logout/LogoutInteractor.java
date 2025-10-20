7f77336ceb8ffa5a4bdd0e210c77c9fe6c68411d
Yihang Xu <yihangxu2416@gmail.com>
2025-10-20 14:41:30 -0400

    Trying

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -20,7 +20,9 @@ public class LogoutInteractor implements LogoutInputBoundary {
         // * set the current username to null in the DAO
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
         // * tell the presenter to prepare a success view.
-
+        userDataAccessObject.setCurrentUsername(null);
+        LogoutOutputData logOutOtpt = new LogoutOutputData(userDataAccessObject.getCurrentUsername());
+        logoutPresenter.prepareSuccessView(logOutOtpt);
     }
 }
 
